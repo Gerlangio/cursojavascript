56ef028dbf542a4100f3e84adde3a72d450b7e10
gerlangiosoares <gerlangio@gmail.com>
2022-09-19 08:03:15 -0300

    tw1

--- aula.js
+++ aula.js
@@ -1,2 +1,70 @@
-console.log('Jhonata');
-console.log('Bem vindo ao curso!')
+//console.log('Jhonata');
+//console.log('Bem vindo ao curso!');
+//let idade = 5;
+//console.log(idade);
+//let altura = 180;
+//console.log(altura);
+
+/*
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto);
+
+const valorIngressoAdulto = 20;
+console.log(valorIngressoAdulto);
+*/
+
+///TIPOS PRIMITIVOS///
+/*
+let nome = 'Rafael'; //string literal
+let idade = 22; //number literal
+let estaAprovado = true; //boolean
+let sobrenome ; //undefined
+let corSelecionado = null; //redefinir um valor
+*/
+
+///arrays///
+/*
+let familia = [26,46,50,17];
+
+console.log(familia);
+*/
+/*
+(4) [26, 46, 50, 17]
+0: 26
+1: 46
+2: 50
+3: 17
+length: 4
+*/
+
+/*
+//outro exemplo
+let variavel = [true,45,'Luiz',17];
+console.log(variavel.length);
+//4
+console.log(variavel);
+/**
+(4) [true, 45, 'Luiz', 17]
+0: true
+1: 45
+2: "Luiz"
+3: 17
+length: 4
+ */
+/*
+console.log(variavel[0]);
+//true
+*/
+
+
+///tipos de referências///
+
+//nomeando uma função
+//verbo + substantivo
+//escopo de uma função
+let corSite = "azul";
+function resetarCor(){
+    corSite = "";
+};
+console.log(corSite);
